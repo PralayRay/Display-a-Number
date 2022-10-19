670b50607624f5f2e2e996b67e1ff8df2feda4ce
Pralay Ray <Pray354@d127.org>
2022-10-19 09:29:06 -0500

    Initial Commit

---
+++ Display a Number/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Display a Number
+//
+//  Created by Pralay T Ray (Student) on 10/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Display a NumberUITests/Display_a_NumberUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Display_a_NumberUITestsLaunchTests.swift
+//  Display a NumberUITests
+//
+//  Created by Pralay T Ray (Student) on 10/19/22.
+//
+
+import XCTest
+
+class Display_a_NumberUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
